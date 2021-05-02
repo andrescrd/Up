59f93261cd0689c931a7b3297dd5d6da9f0ce132
andrescrd <c.andresricardo@gmail.com>
2021-05-02 18:36:06 -0300

    add sound manager

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager _instance;
+
+    public SoundManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = this;
+
+            return _instance;
+        }
+    }
+
+    [SerializeField] private AudioSource start, end, coin, jump;
+
+    public void GameStart() => start.Play();
+    public void GameEnd() => end.Play();
+    public void Coin() => coin.Play();
+    public void Jump() => jump.Play();
+}
